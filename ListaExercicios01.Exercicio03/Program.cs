e543fdd4082b57df3e26c5abebbd38977130ca33
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-09 17:01:22 -0300

    Finalizado terceiro exercício.

---
+++ ListaExercicios01.Exercicio03/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ListaExercicios01.Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa para calcular o volume de um cilindro 
+
+            double raio, altura, volume;
+
+            Console.WriteLine("Para calcular o volume de um cilindro forceça os seguintes dados:");
+
+            Console.WriteLine();
+
+            Console.Write("Raio: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            volume = 3.14 * (raio * raio) * altura;
+
+            Console.Write("O volume do cilindro é: " + volume);
+            Console.ReadLine();
+        }
+    }
+}
