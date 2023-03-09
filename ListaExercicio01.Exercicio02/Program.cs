529239b40b8142c77b467bc7e8cf60ff6a202166
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-09 16:39:25 -0300

    Finalizado exercicio 02.

---
+++ ListaExercicio01.Exercicio02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ListaExercicio01.Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mudar a temperatura de fahrenheit para celsius
+
+            double fahrenheit, celsius;
+
+            Console.WriteLine("Digite a temperatura em F° para saber a temperatura em C°");
+
+            Console.WriteLine("Quantos F° está fazendo neste momento?");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            celsius = (fahrenheit - 32.0) / 1.8;
+            celsius = Math.Round(celsius, 2);
+
+            Console.WriteLine("Hoje fazendo " + celsius + "°C neste momento");
+            Console.ReadLine();
+        }
+    }
+}
