ae1d5d36777b0c3eede29d5417942361537d69c6
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-09 23:18:44 -0300

    Finalizado exercício 08.

---
+++ ListaExercicios01.Exercicio08/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ListaExercicios01.Exercicio08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa para calcular o volume de uma lata de oleo
+
+            double raio, altura, volume;
+
+            Console.WriteLine("Para calcular o volume de uma lata de óleo forneça os seguintes dados:");
+
+            Console.WriteLine();
+
+            Console.Write("Raio: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            volume = 3.14 * (raio * raio) * altura;
+
+            Console.Write("O volume da lata de óleo é: " + volume);
+            Console.ReadLine();
+        }
+    }
+}
