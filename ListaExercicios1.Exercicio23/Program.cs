a0bfa607448fa8e73cf737215a8a38b00f2513e8
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 11:54:57 -0300

    Finalizando o exercício 23.

---
+++ ListaExercicios1.Exercicio23/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ListaExercicios1.Exercicio23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ver todos os números impares de 0 a 100
+
+            Console.WriteLine("Aperte enter para ver todos os números impares de 0 a 100:");
+
+            for (int i = 1; i <= 100; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
