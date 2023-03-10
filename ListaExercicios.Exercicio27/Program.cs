8fb0914a2c82e625501da2710f226a5e21bc1ee6
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 15:22:28 -0300

    Finalizado exercício 27.

---
+++ ListaExercicios.Exercicio27/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ListaExercicios.Exercicio27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fibonacci
+
+            int primeiron = 0, segundon = 1, qtd = 20;
+
+            Console.Write(primeiron + ", " + segundon + ", ");
+
+            for (int i = 2; i < qtd; i++) { 
+                int proximonumero = primeiron + segundon;
+                Console.Write(proximonumero + ", ");
+                primeiron = segundon;
+                segundon = proximonumero;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
