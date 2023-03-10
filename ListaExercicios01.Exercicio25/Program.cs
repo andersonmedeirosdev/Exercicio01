e80e20c55d84405ed903a3dad76499c39bf02f57
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 12:25:35 -0300

    Finalizando o exercício 25.

---
+++ ListaExercicios01.Exercicio25/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ListaExercicios01.Exercicio25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer a taboada de 10
+
+            Console.WriteLine("Digite enter pra ver a tábuada de 10: \n");
+            Console.ReadLine();
+
+        for (int i = 0; i <= 10; i++)
+            {
+                int tabuada = i * 10;
+
+                Console.WriteLine(i + "X" + i + "=" + tabuada);
+            }
+        }
+    }
+}
