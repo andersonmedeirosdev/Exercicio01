a84770ddd4a00dbf0ac092cdd5ea270c7ab4b62d
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 00:35:08 -0300

    Finalizado exercício 11.

---
+++ ListaExercicios01.Exercicio11/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ListaExercicios01.Exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //Programa para saber se um número é primo ou não 
+
+            int numero;
+            bool primo = true;
+
+            Console.Write("Digite um número para saber se ele é primo: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if (numero < 2)
+            {
+                primo = false;
+            } else
+            {
+                for (int i = 2; i <= numero / 2; i++)
+                {
+                    if (numero % i == 0) {
+                        primo = false;
+                        break;
+                    }
+                }
+            }
+
+            if (primo)
+            {
+                Console.WriteLine("Este número é primo.");
+            }
+            else
+            {
+                Console.WriteLine("Este número não é primo.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
