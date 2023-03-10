0c45112f9abb209759d4b36638af8011e6b74878
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 11:25:30 -0300

    Finalizando o exercício 20.

---
+++ ListaExercicios01.Exercicio20/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ListaExercicios01.Exercicio20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verificar se um número é par ou impar 
+
+            int number;
+
+            Console.Write("Digite um número para saber se é impar ou par: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("O número é par.");
+            } else
+            {
+                Console.WriteLine("O número é impar.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
