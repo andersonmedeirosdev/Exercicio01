c6f120e70a63088de3303e9884d75619d359d024
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 14:40:32 -0300

    Exercício 25.

--- ListaExercicios01.Exercicio25/Program.cs
+++ ListaExercicios01.Exercicio25/Program.cs
@@ -6,14 +6,16 @@
         {
             //Fazer a taboada de 10
 
-            Console.WriteLine("Digite enter pra ver a tábuada de 10: \n");
-            Console.ReadLine();
+            Console.WriteLine("Digite um número para ver a tábuada de 10: \n");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
         for (int i = 0; i <= 10; i++)
-            {
-                int tabuada = i * 10;
+            {   
 
-                Console.WriteLine(i + "X" + i + "=" + tabuada);
+                int tabuada = i * number;
+
+                Console.WriteLine(i + "X" + number + "=" + tabuada);
             }
         }
     }
---
+++ ListaExercicios01.Exercicio26/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ListaExercicios01.Exercicio26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sequencia de valores do calculo de A!
+            int a;
+
+            Console.WriteLine("Digite o valor de A para ver a sequêncie do calculo: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = a -1 ; i >= 0; i--)
+            {
+                a = a * i;
+            }
+
+            Console.Write(a);
+
+            Console.ReadLine();
+        }
+    }
+}
