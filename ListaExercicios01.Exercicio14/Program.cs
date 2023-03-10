8ea22744cc829b110e181ef782cccc50ed3a2440
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 01:15:33 -0300

    Finalizando exercício 14.

---
+++ ListaExercicios01.Exercicio14/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ListaExercicios01.Exercicio14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular quantos dias de vida uma pessoa tem.
+
+            string nome;
+            int anos, diasdevida;
+
+            Console.WriteLine("Qual é o seu nome?");
+            nome = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Adicione quantos anos você tem para saber quantos dias você tem de vida: ");
+            anos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            diasdevida = 365 * anos;
+
+            Console.WriteLine(nome + "você tem " + diasdevida + " dias de vida");
+            Console.ReadLine();
+        }
+    }
+}
