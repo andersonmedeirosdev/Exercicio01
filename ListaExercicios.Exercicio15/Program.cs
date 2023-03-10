8c520dbebf78a423e3b2ca268faecbff82a65e05
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 01:22:21 -0300

    Finalizando o exercício 15.

---
+++ ListaExercicios.Exercicio15/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ListaExercicios.Exercicio15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa para mudar de celsius para Fahrenheit
+
+            double fahrenheit, celsius;
+
+            Console.WriteLine("Para saber a temperatura em F°, digite os dados requeridos: ");
+
+            Console.WriteLine("Qual a temperatura em C°");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            fahrenheit = (celsius * 1.8) + 32;
+
+            Console.WriteLine();
+
+            Console.WriteLine(fahrenheit + "°F");
+            Console.ReadLine();
+        }
+    }
+}
