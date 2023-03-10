41006f6f9231d12f3752cffc0eb364a02af67c2a
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 12:12:19 -0300

    Finalizando o exercício 24.

---
+++ ListaExercicios01.Exercicio24/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ListaExercicios01.Exercicio24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ver todos os números impares de 0 a 100
+
+            Console.WriteLine("Aperte enter para saber a soma dos números impares de 0 a 500: \n");
+
+            int soma = 0;
+            for (int i = 1; i <= 500; i += 2)
+            {
+                soma += i;
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("A soma dos números impares de 0 a 500 é: " + soma);
+
+        }
+    }
+}
