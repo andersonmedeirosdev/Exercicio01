5251c3f683e2b0e1c184c3d23483d20464c247ae
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-10 00:47:22 -0300

    Finalizando exercício 12.

---
+++ ListaExercicios01.Exercicio12/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ListaExercicios01.Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exibir a área do terreno.
+
+            double largura, comprimento, area;
+
+            Console.WriteLine("Forneça as seguintes informações para saber a área do terreno: \n");
+
+            Console.Write("Comprimento: ");
+            comprimento = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Largura: ");
+            largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            area = comprimento * largura;
+
+            Console.WriteLine("A area do terreno da Imobiliária Imóbilis é: " + area + " metros");
+            Console.ReadLine();
+        }
+    }
+}
